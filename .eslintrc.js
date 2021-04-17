0a7dba746cf653e65879b668bf195abed927fdab
chaolitan <ctan7749@gmail.com>
2021-04-16 23:30:06 -0700

    config eslint with RN convention

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    'react-native/react-native': true, // whitelist all browser-like globals
   },
   extends: [
     'plugin:react/recommended',
@@ -17,7 +18,13 @@ module.exports = {
   },
   plugins: [
     'react',
+    'react-native', // add eslint-plugin-react-native as a plugin for ESLint
   ],
   rules: {
+    'no-use-before-define': ['error', { functions: true, classes: true, variables: false }], // disable the rule for variables, but enable it for functions and classes
+    // 'react-native/no-unused-styles': 2, // disallow unused styles
+    // 'react-native/no-inline-styles': 2, // disallow styles declared within the component itself
+    // 'react-native/no-color-literals': 2, // enforces variable names to be used for storing colors
+
   },
 };
--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -1,30 +1,22 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import HomeMap from '../../components/HomeMap';
 import CovidMessages from '../../components/CovidMessages';
-import HomeSearch from '../../components/HomeSearch'
+import HomeSearch from '../../components/HomeSearch';
 
-const HomeScreen = () => {
-  return (
-    <View>
-      <HomeMap />
-      {/* Covid Msg */}
-      <CovidMessages />
-      {/* Learn More */}
-      <HomeSearch></HomeSearch>
-    </View>
-  );
-};
+const HomeScreen = () => (
+  <View>
+    <HomeMap />
+    {/* Covid Msg */}
+    <CovidMessages />
+    {/* Learn More */}
+    <HomeSearch />
+  </View>
+);
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    height: 300,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'lightblue',
-  },
+
 });
