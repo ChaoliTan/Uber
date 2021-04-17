886d8469fd2a2476fa1dd006da0910c93be9d37b
chaolitan <ctan7749@gmail.com>
2021-04-16 23:18:04 -0700

    eslint --init

--- src/components/HomeSearch/index.jsx
+++ src/components/HomeSearch/index.jsx
@@ -1,43 +1,50 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet, Text, View, Image,
+} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-const index = () => {
-  return (
-    <View>
-      {/* input text */}
-      <View style={styles.inputBox}>
-        <Text style={styles.inputText}>Where To?</Text>
-        <View style={styles.timeContainer}>
-          <AntDesign name={'clockcircle'} size={16} color={'#535353'}></AntDesign>
-          <Text>Now</Text>
-          <MaterialIcons name={'keyboard-arrow-down'} size={16}></MaterialIcons>
-        </View>
+const HomeSearch = () => (
+  <View>
+    {/* input text */}
+    <View style={styles.inputBox}>
+      <Text style={styles.inputText}>Where to?</Text>
+      <View style={styles.timeContainer}>
+        <AntDesign name="clockcircle" size={16} color="#535353" />
+        <Text>Now</Text>
+        <MaterialIcons name="keyboard-arrow-down" size={16} />
       </View>
-
-      {/* previous destination */}
-
-      {/* home destination */}
     </View>
-  );
-};
 
-export default index;
+    {/* previous destination */}
+    <View style={styles.row}>
+      <View style={styles.iconContainer}>
+        <AntDesign />
+        <Image />
+      </View>
+      <Text>Spin Nightclub</Text>
+    </View>
+
+    {/* home destination */}
+  </View>
+);
+
+export default HomeSearch;
 
 const styles = StyleSheet.create({
   inputBox: {
-    backgroundColor: '#d9d9d9',
+    backgroundColor: '#e7e7e7',
     margin: 10,
     padding: 10,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   inputText: {
     fontSize: 20,
     fontWeight: '600',
-    color: '#6e6e6e'
+    color: '#434343',
   },
   timeContainer: {
     flexDirection: 'row',
