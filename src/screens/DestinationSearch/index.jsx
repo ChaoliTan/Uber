9316755aa21630fe4dec0f6e7dfe37ce100cda36
chaolitan <ctan7749@gmail.com>
2021-04-23 07:48:23 -0700

    finish search page

--- src/screens/DestinationSearch/index.jsx
+++ src/screens/DestinationSearch/index.jsx
@@ -1,17 +1,46 @@
 import React, { useState } from 'react';
 import {
-  StyleSheet, SafeAreaView, TextInput, View,
+  StyleSheet, SafeAreaView, TextInput, View, Text,
 } from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 const DestinationSearch = () => {
-  const [fromText, setFromText] = useState('');
-  const [destinationText, setDestinationText] = useState('');
+  const [originPlace, setOriginPlace] = useState(null);
+  const [destinationPlace, setDestinationPlace] = useState('');
 
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        <TextInput value={fromText} onChangeText={setFromText} style={styles.textInput} placeholder="From" />
-        <TextInput value={destinationText} onChangeText={setDestinationText} style={styles.textInput} placeholder="Where to" />
+        <GooglePlacesAutocomplete
+          placeholder="From where"
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            setOriginPlace({ data, details });
+          }}
+          styles={{
+            textInput: styles.textInput,
+          }}
+          fetchDetails
+          query={{
+            key: 'AIzaSyDX0rNKcUIjZnTu2VBFDiKLkbwkE9o6MFI',
+            language: 'en',
+          }}
+        />
+        <GooglePlacesAutocomplete
+          placeholder="Where to?"
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            setDestinationPlace({ data, details });
+          }}
+          styles={{
+            textInput: styles.textInput,
+          }}
+          fetchDetails
+          query={{
+            key: 'AIzaSyDX0rNKcUIjZnTu2VBFDiKLkbwkE9o6MFI',
+            language: 'en',
+          }}
+        />
       </View>
     </SafeAreaView>
 
@@ -23,6 +52,7 @@ export default DestinationSearch;
 const styles = StyleSheet.create({
   container: {
     padding: 10,
+    height: '100%',
   },
   textInput: {
     padding: 10,
