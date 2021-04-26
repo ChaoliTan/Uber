cc7eed1b4489d1c546efa71dabbdd69944b85c3d
chaolitan <ctan7749@gmail.com>
2021-04-26 00:26:12 -0700

    add react-native-community/geolocation

--- App.js
+++ App.js
@@ -7,17 +7,55 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
+import {PermissionsAndroid, Platform} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
 import DestinationSearch from './src/screens/DestinationSearch';
 import HomeScreen from './src/screens/HomeScreen';
 import SearchResults from './src/screens/SearchResults';
 
-const App = () => (
-    <>
-        {/* <HomeScreen /> */}
-        <DestinationSearch />
-        {/* <SearchResults /> */}
-    </>
-);
+navigator.geolocation = require('@react-native-community/geolocation');
 
+const App = () => {
+    const androidPermission = async () => {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                {
+                    title: 'Uber App Camera Permission',
+                    message:
+                        'Uber App needs access to your location ' +
+                        'so you can take awesome rides.',
+                    buttonNeutral: 'Ask Me Later',
+                    buttonNegative: 'Cancel',
+                    buttonPositive: 'OK',
+                },
+            );
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                console.log('You can use the location');
+            } else {
+                console.log('Location permission denied');
+            }
+        } catch (err) {
+            console.warn(err);
+        }
+    };
+
+    useEffect(() => {
+        if (Platform.OS === 'android') {
+            androidPermission();
+        } else {
+            // IOS
+            Geolocation.requestAuthorization();
+        }
+    }, []);
+
+    return (
+        <>
+            {/* <HomeScreen /> */}
+            <DestinationSearch />
+            {/* <SearchResults /> */}
+        </>
+    );
+};
 export default App;
