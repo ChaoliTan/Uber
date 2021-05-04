b37134d50553a10261aaf8774cb63c41e923e71e
chaolitan <ctan7749@gmail.com>
2021-05-03 20:16:25 -0700

    navigation done

--- src/navigation/Root.jsx
+++ src/navigation/Root.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
-// import {NavigationContainer} from '@react-navigation/native';
-import HomeScreen from '../screens/HomeScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const Root = () => {
+import HomeScreen from '../screens/HomeScreen';
+import DestinationSearch from '../screens/DestinationSearch';
+import SearchResults from '../screens/SearchResults';
+
+const Stack = createStackNavigator();
+
+const RootNavigator = () => {
     return (
         <>
-            <HomeScreen />
+            <NavigationContainer>
+                <Stack.Navigator
+                    screenOptions={{
+                        headerShown: false,
+                    }}
+                    initialRouteName='DestinationSearch'>
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <Stack.Screen
+                        name="DestinationSearch"
+                        component={DestinationSearch}
+                    />
+                    <Stack.Screen
+                        name="SearchResults"
+                        component={SearchResults}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
         </>
     );
 };
 
-export default Root;
+export default RootNavigator;
